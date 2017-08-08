3cc055b10a4a2f76f08dda154f989b09be9520e3
Madeline Ford <madelineford@macintosh.attlocal.net>
2017-08-07 20:16:56 -0700

    first commit: added basic html layout, css, reset css, started pseudocoding for js/jquery

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+//The function for when the page loads
+$(document).ready(function() {
+
+	//function for choosing a random number at the beginning of the game, between 19 and 120
+	computerNumber(Math.floor(Math.random) * 120) //do I need to multiply this by an array?
+	//This is the part for printing the random number to the screen
+	function printComputerNumber() {
+		document
+	}
+
+}
