ba065c230cc0e1dd9a229d286e95230088d57a9b
Madeline Ford <madelineford@calvisitor-10-105-170-167.calvisitor.1918.berkeley.edu>
2017-08-12 14:29:38 -0700

    game is functional, just not pretty yet

--- assets/js/game.js
+++ assets/js/game.js
@@ -38,39 +38,42 @@ console.log(crystal);
 //Start of game play
 $(document).ready(function() {
 	
-	(function setupAndReset() {
-		crystalGame.wins = 0;
-		crystalGame.losses = 0;
+	function setupAndReset() {
+		//{type, id} = crystalGame
+		//crystalGame.wins = 0;
+		//crystalGame.losses = 0;
 		crystalGame.playerNumber = 0;
 		crystalGame.computerNumber = Math.floor(Math.random() * (120 - 19)) + 19;
 			console.log("this is the random number", crystalGame.computerNumber);
 
 		//crystal.value = Math.Floor(Math.random() (12 - 1)) + 1; //how would I set this up for each one?
 
-		$("#computernumber").html(crystalGame.computerNumber);
+		$("#targetnumber").html(crystalGame.computerNumber);
 		//console.log(computerNumber);
-		$("#playernumber").html(crystalGame.playerNumber);
+		$("#updatedplayernumber").html(crystalGame.playerNumber);
 		//$(".crystals").html(crystal.value);  <--maybe show these after the first click?
-		$(".wins").append(crystalGame.wins);
-		$(".losses").append(crystalGame.losses);
-		$("#prompt").text("Click a crystal to start!")
+		$("#winnumber").html(crystalGame.wins);
+		$("#lossnumber").html(crystalGame.losses);
+		$("#prompt").html("Click a crystal to start!")
 		scoreCheck()
 
-	})();
+	}
+
+	setupAndReset();
 	
 
 	function scoreCheck() {
 		if (crystalGame.playerNumber === crystalGame.computerNumber) {
 			crystalGame.wins++;
-			$("#prompt").text("You won!");
-			$("#playerNumber").append(playerNumber);
+			$("#prompt").html("You won!");
+			$("#updatedplayernumber").append(crystalGame.playerNumber);
 			//some kind of animation with glitter?
 			setupAndReset()
 		}
 		else if (crystalGame.playerNumber > crystalGame.computerNumber) {
 			crystalGame.losses++;
-			$("#prompt").text("You lost. Try again!");
-			$("#playerNumber").append(crystalGame.playerNumber);
+			$("#prompt").html("You lost. Try again!");
+			$("#updatedplayernumber").append(crystalGame.playerNumber);
 			setupAndReset()
 		}
 	};
@@ -81,32 +84,36 @@ $(document).ready(function() {
 		crystalGame.playerNumber += crystal[0].value;
 		//playerNumber = playerNumber + crystal[0].value; //check naming of this, might not be calling correctly; how to call name above?
 		scoreCheck();
-		$("#playerNumber").append(crystalGame.playerNumber + crystal[0].value);
+		$("#updatedplayernumber").html(crystalGame.playerNumber);
 		console.log(crystal[0].value);
+		console.log("whitecrystal", crystalGame.playerNumber);
 	});
 
 	$(".blue").on("click", function() {
 		crystalGame.playerNumber += crystal[1].value;
 		//playerNumber = playerNumber + crystal[1].value; //check naming of this, might not be calling correctly; how to call name above?
 		scoreCheck();
-		$("#playerNumber").append(crystalGame.playerNumber + crystal[1].value);
+		$("#updatedplayernumber").html(crystalGame.playerNumber);
 		console.log(crystal[1].value);
+		console.log("bluecrystal", crystalGame.playerNumber);
 	});
 
 	$(".green").on("click", function() {
 		crystalGame.playerNumber += crystal[2].value;
 		//playerNumber = playerNumber + crystal[2].value; //check naming of this, might not be calling correctly; how to call name above?
 		scoreCheck();
-		$("#playerNumber").append(crystalGame.playerNumber + crystal[2].value);
+		$("#updatedplayernumber").html(crystalGame.playerNumber);
 		console.log(crystal[2].value);
+		console.log("greencrystal", crystalGame.playerNumber);
 	});
 
 	$(".orange").on("click", function() {
 		crystalGame.playerNumber += crystal[3].value;
 		//playerNumber = playerNumber + crystal[3].value; //check naming of this, might not be calling correctly; how to call name above?
 		scoreCheck();
-		$("#playerNumber").append(crystalGame.playerNumber + crystal[3].value);
+		$("#updatedplayernumber").html(crystalGame.playerNumber);
 		console.log(crystal[3].value);
+		console.log("orangecrystal", crystalGame.playerNumber);
 	});
 	//basically same as above, only without resetting wins and losses?
 	// function reset () {
