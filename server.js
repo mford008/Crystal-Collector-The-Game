a24a080922ce9195d2709da2c551c4b3455813d8
Maddy Ford <mrhf008@gmail.com>
2017-12-21 19:54:51 -0800

    added server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = 8080;
+
+// require('./assets');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static('public'));
+// require('./assets/css')(app);
+// require('./assets/images')(app);
+// require('./assets/js')(app);
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, function () {
+  console.log('App listening on PORT ' + PORT);
+});
