3ca0a61bf8a24b08bc8e09745d034345d1cfe735
Maddy Ford <mrhf008@gmail.com>
2017-12-24 08:48:32 -0800

    cleaned up spacing, comments

--- server.js
+++ server.js
@@ -5,16 +5,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// require('./assets');
-
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(express.static('public'));
-// require('./assets/css')(app);
-// require('./assets/images')(app);
-// require('./assets/js')(app);
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
